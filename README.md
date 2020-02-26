0b10760f0ab25dac57948f0fd9a8c7be750be037
Regina Orozco <regina.orozco91@gmail.com>
2020-02-26 12:40:09 -0600

    leer archivo y extraer links

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 README.md con descripción del módulo, instrucciones de instalación/uso, documentación del API y ejemplos. Todo lo relevante para que cualquier developer que quiera usar tu librería pueda hacerlo sin inconvenientes.
 
-Pseudo codigo o diagrama de flujo con el algoritmo que soluciona el problema.
- Un board con el backlog para la implementación de la librería.
- Documentación técnica de la librería.
- Guía de uso e instalación de la librería
+1. Pseudo codigo o diagrama de flujo con el algoritmo que soluciona el problema.
+
+![Diagrama de flujo](https://raw.githubusercontent.com/parzibyte/WaterPy/master/assets/ImagenV1.png)
+2. Un board con el backlog para la implementación de la librería.
+3. Documentación técnica de la librería.
+4. Guía de uso e instalación de la librería
---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+const process = require("process");
+const archivoMD  = process.argv[2];
+
+
+const fileValidation = () => {
+    let extension = path.extname(archivoMD);
+    if (extension === ".md") {
+        console.log("Este es un archivo extensión " + extension);
+        return true;
+    } else {
+        console.error("Archivo inválido, su extensión es " + extension + ", por favor ingrese un archivo .md")
+          return false;
+    }
+}
+console.log(fileValidation(archivoMD));
+
+
+  const readMD = fs.readFile(archivoMD, "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+
+    } else {
+      const regex = new RegExp(/https?:\/\/[\w\.\-]+\.\w{2,5}[^\s\)]+/g);
+       const links = data.match(regex);
+       console.log(links);
+    }
+  });
